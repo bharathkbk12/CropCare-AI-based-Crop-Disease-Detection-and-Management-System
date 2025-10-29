23f4d92704f25effa4a5703ca4d0599f74237163
bharathkbk12 <bharathkbk86@gmail.com>
2025-10-29 23:32:54 +0530

    Add files via upload

---
+++ app/models/fertilizer.py
@@ -0,0 +1,101 @@
+fertilizer_dic = {
+    'NHigh': """The N value of soil is high and might give rise to weeds.
+    <br/>Please consider the following suggestions:
+
+    <br/><br/>1. <i>Manure</i> – Adding manure is one of the simplest ways to amend your soil with nitrogen. Be careful as there are various types of manures with varying degrees of nitrogen.
+
+    <br/>2. <i>Coffee grinds</i> – Use your morning addiction to feed your gardening habit! Coffee grinds are considered a green compost material which is rich in nitrogen. Once the grounds break down, your soil will be fed with nitrogen. An added benefit of including coffee grounds in your soil is improved drainage.
+
+    <br/>3. <i>Plant nitrogen-fixing plants</i> – Vegetables in the Fabaceae family like peas, beans, and soybeans can increase nitrogen in your soil.
+
+    <br/>4. Plant ‘green manure’ crops like cabbage, corn, and broccoli.
+
+    <br/>5. <i>Use mulch (wet grass) while growing crops</i> – Mulch can include sawdust and scrap softwoods.
+    """,
+
+    'Nlow': """The N value of your soil is low.
+    <br/>Please consider the following suggestions:
+
+    <br/><br/>1. <i>Add sawdust or fine woodchips to your soil</i> – The carbon in the sawdust/woodchips loves nitrogen and will help absorb excess nitrogen.
+
+    <br/>2. <i>Plant heavy nitrogen-feeding plants</i> – Tomatoes, corn, broccoli, cabbage, and spinach thrive on nitrogen and will reduce its concentration.
+
+    <br/>3. <i>Water</i> – Soaking your soil with water will leach nitrogen deeper into the soil, leaving less for plants to use.
+
+    <br/>4. <i>Sugar</i> – Limited studies show adding sugar can reduce nitrogen levels. Sugar's carbon content attracts and binds nitrogen, similar to sawdust or woodchips.
+
+    <br/>5. Add composted manure to the soil.
+
+    <br/>6. Plant nitrogen-fixing plants like peas or beans.
+
+    <br/>7. <i>Use NPK fertilizers with high N values.</i>
+
+    <br/>8. <i>Do nothing</i> – If you already have plants producing lots of foliage, let them absorb the nitrogen for amending the soil for future crops.
+    """,
+
+    'PHigh': """The P value of your soil is high.
+    <br/>Please consider the following suggestions:
+
+    <br/><br/>1. <i>Avoid adding manure</i> – Manure contains high levels of phosphorus. Limiting manure will reduce phosphorus input.
+
+    <br/>2. <i>Use phosphorus-free fertilizer</i> – Limit phosphorus while providing other nutrients such as nitrogen and potassium. Look for fertilizers with numbers like 10-0-10.
+
+    <br/>3. <i>Water your soil</i> – Soaking your soil liberally can help reduce phosphorus levels, though this should be a last resort.
+
+    <br/>4. Plant nitrogen-fixing vegetables like beans and peas to increase nitrogen without raising phosphorus.
+
+    <br/>5. Use crop rotations to decrease phosphorus levels.
+    """,
+
+    'Plow': """The P value of your soil is low.
+    <br/>Please consider the following suggestions:
+
+    <br/><br/>1. <i>Bone meal</i> – A fast-acting source made from ground animal bones rich in phosphorus.
+
+    <br/>2. <i>Rock phosphate</i> – A slower-acting source requiring soil to convert it into usable phosphorus.
+
+    <br/>3. <i>Phosphorus fertilizers</i> – Use fertilizers with high phosphorus in the NPK ratio (e.g., 10-20-10).
+
+    <br/>4. <i>Organic compost</i> – Adding quality organic compost will increase phosphorus content.
+
+    <br/>5. <i>Manure</i> – An excellent source of phosphorus, similar to compost.
+
+    <br/>6. <i>Clay soil</i> – Introducing clay particles can help retain phosphorus.
+
+    <br/>7. <i>Ensure proper soil pH</i> – pH levels between 6.0 and 7.0 optimize phosphorus uptake in plants.
+
+    <br/>8. If pH is low, add lime or potassium carbonate as fertilizers. Calcium carbonate effectively raises pH.
+
+    <br/>9. If pH is high, add organic matter to acidify the soil. Acidifying fertilizers like ammonium sulfate can lower soil pH.
+    """,
+
+    'KHigh': """The K value of your soil is high.
+    <br/>Please consider the following suggestions:
+
+    <br/><br/>1. <i>Loosen the soil</i> deeply with a shovel, then water thoroughly to dissolve potassium. Repeat the process two or three times.
+
+    <br/>2. <i>Sift through the soil</i> to remove rocks that slowly release potassium.
+
+    <br/>3. Stop using potassium-rich fertilizers. Use only fertilizers with a '0' in the last field (e.g., 10-0-10) or switch to organic matter.
+
+    <br/>4. Add crushed eggshells, seashells, wood ash, or soft rock phosphate to add calcium.
+
+    <br/>5. Use NPK fertilizers with low potassium levels or organic fertilizers with low NPK values.
+
+    <br/>6. Grow a cover crop of legumes to fix nitrogen without increasing potassium.
+    """,
+
+    'Klow': """The K value of your soil is low.
+    <br/>Please consider the following suggestions:
+
+    <br/><br/>1. Mix in muriate of potash or sulfate of potash.
+
+    <br/>2. Use kelp meal or seaweed.
+
+    <br/>3. Apply Sul-Po-Mag.
+
+    <br/>4. Bury banana peels an inch below the soil's surface.
+
+    <br/>5. Use potash fertilizers since they contain high potassium levels.
+    """
+}
